0837e74b33ebef386af44ab040a616d2f5f26099
fess89 <fess89@gmail.com>
2014-03-02 15:55:37 +0200

    Enabled assets precompile

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module Rosvibory
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Kyiv'
 		config.active_record.schema_format = :sql
-		config.assets.initialize_on_precompile = false
+		#config.assets.initialize_on_precompile = false
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
