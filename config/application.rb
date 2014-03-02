bbc3e70273bce2edca45170831aecef2fde1b5e0
fess89 <fess89@gmail.com>
2014-03-02 17:23:49 +0200

    Changed production.rb to serve assets correctly

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module Rosvibory
     config.time_zone = 'Kyiv'
 		config.active_record.schema_format = :sql
 		#config.assets.initialize_on_precompile = false
-
+		config.assets.enabled = true
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ru
